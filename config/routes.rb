aa7988c2c767db6c988781904aabc270d4f8bf25
Gilles Mathurin <gillesmath@me.com>
2009-09-21 19:00:31 -0400

    ajout d'images et debut de mise en page avec actualisation des feuilles de styles

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ ActionController::Routing::Routes.draw do |map|
                                      :purge     => :delete } 
 
   map.resource :session
+  
+  map.root :controller => "home", :action => "index"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
