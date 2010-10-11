cf5066bac704523622a770f955d2d3cc257ef4f0
Gilles MATHURIN <gillesmath@macmini.local>
2010-10-11 11:54:27 -0400

    nettoyage

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,5 +1,9 @@
 class JavascriptsController < ApplicationController
   def masquer_annonce
     session[:announcement_hide_time] = Time.now
+    # debugger
+    # Annonce.all.each do |annonce|
+    #   annonce.update_attribute(:updated_at, Time.now)
+    # end
   end
 end
